78c4f1bea159fcebdcce099e075c7bf9c698892b
Adrian Johnson <Adrian.Johnson@excelerateds2p.com>
2024-02-09 16:54:45 +0000

    .gitignore build

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export NUMCPUS=`grep -c '^processor' /proc/cpuinfo` &&
-mkdir -p /usr/share/sdrpp &&
-mkdir -p build &&
-cd build &&
-cmake .. &&
-make -j$NUMCPUS --load-average=$NUMCPUS
